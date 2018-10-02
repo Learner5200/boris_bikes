9a665daf63cb52a718c203f4d9d110f4ac142606
Anouska Hopkins <anouskahopkins@gmail.com>
2018-10-02 16:46:31 +0100

    Add private methods for refactoring

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -4,13 +4,33 @@ class DockingStation
     @bikes = []
   end
 
+  private
+
+  def empty?
+    if @bikes.empty?
+      true
+    else
+      false
+    end
+  end
+
+  def full?
+    if @bikes.length >= 20
+      true
+    else
+      false
+    end
+  end
+
+  public
+
   def dock(bike)
-    fail("station is full!") if @bikes.length >= 20
+    fail("station is full!") if full?
     @bikes << bike
   end
 
   def release_bike
-    fail("station is empty!") if @bikes.empty?
+    fail("station is empty!") if empty?
     @bikes.pop
   end
 
