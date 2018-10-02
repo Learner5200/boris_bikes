f4b3bf7bd2c877d2cc12ea09f950494946600d57
Chris Cooksley <christopher.cooksley@gmail.com>
2018-10-02 14:10:43 +0100

    renamed some tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,7 @@
 require "boris_bikes"
 describe DockingStation do
   it { should respond_to :release_bike }
-  it "get bike and expect working" do
+  it "releases working bikes" do
     expect(subject.release_bike).to be_working
   end
 end
