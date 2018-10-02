1db476092e281cefde488211d10fc90e94ce5a1e
Chris Cooksley <christopher.cooksley@gmail.com>
2018-10-02 16:06:23 +0100

    refactored tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,15 @@
 require "boris_bikes"
 describe DockingStation do
+
   it { should respond_to :release_bike }
+
+  it { should respond_to(:dock).with(1).argument }
+
+  it "docks a bike" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
 
   it "releases bikes" do
     bike = Bike.new
@@ -14,19 +23,8 @@ describe DockingStation do
     expect(subject.release_bike).to be_working
   end
 
-  it { should respond_to(:dock).with(1).argument }
-
-  it "docks a bike" do
-    bike = Bike.new
-    station = subject
-    station.dock(bike)
-    expect(station.bike).to eq(bike)
-  end
-
   it "raises an error when docking station is empty" do
-    station = DockingStation.new
-    station.bike = nil
-    expect { station.release_bike }.to raise_error("station is empty!")
+    expect { subject.release_bike }.to raise_error("station is empty!")
   end
 
 end
